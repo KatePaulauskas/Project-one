66b54f97991c0bf190b7f884db0c9aa6f0fc581a
KatePaulauskas <>
2023-11-29 08:04:51 +0000

    Style contact page, center paragraph text on the projects page

--- process-form.php
+++ process-form.php
@@ -1,12 +1,16 @@
 <?php
+// Start the session
 session_start();
 
-// Collect form data
-$_SESSION['name'] = $_POST['name'];
-$_SESSION['email'] = $_POST['email-address'];
-$_SESSION['message'] = $_POST['add-your-message'];
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Store form data in session variables
+    $_SESSION['name'] = $_POST['name'] ?? '';
+    $_SESSION['email'] = $_POST['email-address'] ?? '';
+    $_SESSION['message'] = $_POST['add-your-message'] ?? '';
 
-// Redirect to the results page
-header('Location: display-results.php');
-exit();
+    // Redirect to the display-results page
+    header('Location: display-results.php');
+    exit();
+}
 ?>
